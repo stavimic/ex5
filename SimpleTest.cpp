14784909c744e7529ff11761624d373473afdfa1
hareld10 <harel91827364>
2018-06-20 18:22:35 +0300

    Merge branch 'master' of https://github.com/stavimic/ex5
    # Conflicts:
    #	VirtualMemory.cpp

--- SimpleTest.cpp
+++ SimpleTest.cpp
@@ -5,23 +5,30 @@
 #include <bitset>
 #include <iostream>
 
+uint64_t getBits(uint64_t addr, int lower, int higher){
+    uint64_t num = (addr >> lower) & ((1 << (higher - lower + 1))-1);
+    return num;
+}
+
 int main(int argc, char **argv) {
-    VMinitialize();
-    for (uint64_t i = 0; i < (2 * NUM_FRAMES); ++i) {
-        printf("writing to %llu\n", i);
-        VMwrite(5 * i * PAGE_SIZE, i);
-    }
+//    VMinitialize();
+//    for (uint64_t i = 0; i < (2 * NUM_FRAMES); ++i) {
+//        printf("writing to %llu\n", i);
+//        VMwrite(5 * i * PAGE_SIZE, i);
+//    }
+//
+//    for (uint64_t i = 0; i < (2 * NUM_FRAMES); ++i) {
+//        word_t value;
+//        VMread(5 * i * PAGE_SIZE, &value);
+//        printf("reading from %llu %d\n", i, value);
+//        assert(uint64_t(value) == i);
+//    }
+//    printf("success\n");
 
-    for (uint64_t i = 0; i < (2 * NUM_FRAMES); ++i) {
-        word_t value;
-        VMread(5 * i * PAGE_SIZE, &value);
-        printf("reading from %llu %d\n", i, value);
-        assert(uint64_t(value) == i);
-    }
-    printf("success\n");
-
-    std::string binary = std::bitset<32>(999990).to_string();
-    std::cout<<binary;
-
+    uint64_t t = 1302;
+    std::cout << getBits(t, 4, 7) << std::endl;
+    std::cout << getBits(t, 1, 3) << std::endl;
+    std::cout << getBits(t, 0, 10) << std::endl;
     return 0;
 }
+
