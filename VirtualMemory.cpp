6fa2be519e306c9f2fe88b42c713ad7f132c75cf
mstav <stav.michaely@mail.huji.ac.il>
2018-06-20 11:44:42 +0300

    first commit

---
+++ VirtualMemory.cpp
@@ -0,0 +1,22 @@
+#include "VirtualMemory.h"
+#include "PhysicalMemory.h"
+
+void clearTable(uint64_t frameIndex) {
+    for (uint64_t i = 0; i < PAGE_SIZE; ++i) {
+        PMwrite(frameIndex * PAGE_SIZE + i, 0);
+    }
+}
+
+void VMinitialize() {
+    clearTable(0);
+}
+
+
+int VMread(uint64_t virtualAddress, word_t* value) {
+    return 1;
+}
+
+
+int VMwrite(uint64_t virtualAddress, word_t value) {
+    return 1;
+}
