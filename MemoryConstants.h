641e60745a216f8984429d3a0ad5c034493f0027
mstav <stav.michaely@mail.huji.ac.il>
2018-06-23 13:57:40 +0300

    forbidden frame

--- MemoryConstants.h
+++ MemoryConstants.h
@@ -1,30 +1,55 @@
-#pragma once
-
-#include <climits>
-#include <stdint.h>
-
-// word
-typedef int word_t;
-
-#define WORD_WIDTH (sizeof(word_t) * CHAR_BIT)
-
-
-
+//#pragma once
+//
+//#include <climits>
+//#include <stdint.h>
+//
+//// word
+//typedef int word_t;
+//
+//#define WORD_WIDTH (sizeof(word_t) * CHAR_BIT)
+//
+//
+//
+////
+////// number of bits in the offset,
+////// in this implementation this is also the number of entries in a table
+////#define OFFSET_WIDTH 2
+////// page/frame size in words
+////// 2^OFFSET_WIDTH
+////#define PAGE_SIZE (1LL << OFFSET_WIDTH)
+////
+////// number of bits in a physical address
+////#define PHYSICAL_ADDRESS_WIDTH 4
+////// RAM size in words
+////#define RAM_SIZE (1LL << PHYSICAL_ADDRESS_WIDTH)
+////
+////// number of bits in a virtual address
+////#define VIRTUAL_ADDRESS_WIDTH 5
+////// virtual memory size in words
+////#define VIRTUAL_MEMORY_SIZE (1LL << VIRTUAL_ADDRESS_WIDTH)
+////
+////// number of frames in the RAM
+////#define NUM_FRAMES (RAM_SIZE / PAGE_SIZE)
+////
+////// number of pages in the virtual memory
+////#define NUM_PAGES (VIRTUAL_MEMORY_SIZE / PAGE_SIZE)
+////
+////#define TABLES_DEPTH ((VIRTUAL_ADDRESS_WIDTH - 1) / OFFSET_WIDTH)
 //
 //// number of bits in the offset,
 //// in this implementation this is also the number of entries in a table
-//#define OFFSET_WIDTH 2
+//#define OFFSET_WIDTH 4
 //// page/frame size in words
 //// 2^OFFSET_WIDTH
 //#define PAGE_SIZE (1LL << OFFSET_WIDTH)
 //
 //// number of bits in a physical address
-//#define PHYSICAL_ADDRESS_WIDTH 4
+//#define PHYSICAL_ADDRESS_WIDTH 10
 //// RAM size in words
 //#define RAM_SIZE (1LL << PHYSICAL_ADDRESS_WIDTH)
 //
 //// number of bits in a virtual address
-//#define VIRTUAL_ADDRESS_WIDTH 5
+//#define VIRTUAL_ADDRESS_WIDTH 11
 //// virtual memory size in words
 //#define VIRTUAL_MEMORY_SIZE (1LL << VIRTUAL_ADDRESS_WIDTH)
 //
@@ -36,12 +61,23 @@ typedef int word_t;
 //
 //#define TABLES_DEPTH ((VIRTUAL_ADDRESS_WIDTH - 1) / OFFSET_WIDTH)
 
+
+#pragma once
+
+#include <climits>
+#include <stdint.h>
+
+// word
+typedef int word_t;
+
+#define WORD_WIDTH (sizeof(word_t) * CHAR_BIT)
+
 // number of bits in the offset,
-// in this implementation this is also the number of entries in a table
 #define OFFSET_WIDTH 4
 // page/frame size in words
-// 2^OFFSET_WIDTH
+// in this implementation this is also the number of entries in a table
 #define PAGE_SIZE (1LL << OFFSET_WIDTH)
+
 
 // number of bits in a physical address
 #define PHYSICAL_ADDRESS_WIDTH 10
@@ -49,7 +85,7 @@ typedef int word_t;
 #define RAM_SIZE (1LL << PHYSICAL_ADDRESS_WIDTH)
 
 // number of bits in a virtual address
-#define VIRTUAL_ADDRESS_WIDTH 11
+#define VIRTUAL_ADDRESS_WIDTH 20
 // virtual memory size in words
 #define VIRTUAL_MEMORY_SIZE (1LL << VIRTUAL_ADDRESS_WIDTH)
 
